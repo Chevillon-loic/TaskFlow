8ddc134e118d5fa7cc97601256f45efaf01fdc13
Chaouki FREJ <chaoukifrej@hotmail.com>
2021-04-24 15:05:53 +0200

    retouche style welcome

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,6 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -16,42 +17,44 @@
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     @yield('custom_styles')
 </head>
+
 <body>
 
     <!-- Navbar -->
     @auth
-    <div id="navbar">
-        <img class="logo" src="{{ asset('img/logoColor.svg') }}" alt="Logo">
-        <nav>
-            @if (Route::has('login'))
-            <div>
-                @auth
-                <a href="{{ url('/home') }}">Home</a>
-                @else
-                <a href="{{ route('login') }}">Connection</a>
-                @if (Route::has('register'))
-                <a href="{{ route('register') }}">Inscription</a>
+        <div id="navbar">
+            <img class="logo" src="{{ asset('img/logoColor.png') }}" alt="Logo">
+            <nav>
+                @if (Route::has('login'))
+                    <div>
+                        @auth
+                            <a href="{{ url('/home') }}">Home</a>
+                        @else
+                            <a href="{{ route('login') }}">Connection</a>
+                            @if (Route::has('register'))
+                                <a href="{{ route('register') }}">Inscription</a>
+                            @endif
+                        @endauth
+                    </div>
                 @endif
-                @endauth
-            </div>
-            @endif
-        </nav>
-    </div>
+            </nav>
+        </div>
     @else
-    <div id="navbarNotConnected">
-        <nav>
-            <div>
-                <a class="connectionBtn" href="{{ route('login') }}">CONNEXION</a>
-                <a class="registerBtn" href="{{ route('register') }}">INSCRIPTION</a>
-            </div>
-        </nav>
-    </div>
+        <div id="navbarNotConnected">
+            <nav>
+                <div>
+                    <a class="connectionBtn" href="{{ route('login') }}">CONNEXION</a>
+                    <a class="registerBtn" href="{{ route('register') }}">INSCRIPTION</a>
+                </div>
+            </nav>
+        </div>
     @endauth
 
-        <main>
-            @yield('content')
-        </main>
+    <main>
+        @yield('content')
+    </main>
 
 
 </body>
+
 </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,21 +1,23 @@
 @extends('layouts.app')
 
 @section('custom_styles')
-<link href="{{ asset('css/welcome.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/welcome.css') }}" rel="stylesheet">
 @endsection
 
 @section('content')
-<div class="WelcomeMainContent">
-    <div class="leftWelcomeMainContent">
-        <img src="{{asset('img/logoColor.svg')}}" alt="Logo">
-        <div class="pWelcomeMainContent">
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla aliquid quia eos temporibus veniam dolorum alias perferendis ad voluptates, inventore ducimus suscipit, molestias totam ea itaque iste, reiciendis quo facere.
-            </p>
+    <div class="WelcomeMainContent">
+        <div class="leftWelcomeMainContent">
+            <img src="{{ asset('img/logoColor.png') }}" alt="Logo">
+            <div class="pWelcomeMainContent">
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla aliquid quia eos temporibus veniam
+                    dolorum alias perferendis ad voluptates, inventore ducimus suscipit, molestias totam ea itaque iste,
+                    reiciendis quo facere.
+                </p>
+            </div>
+        </div>
+        <div class="rightWelcomeMainContent">
+            <img src="{{ asset('img/Humaaans - 3 Characters.png') }}" alt="Logo">
         </div>
     </div>
-    <div class="rightWelcomeMainContent">
-        <img src="{{asset('img/Humaaans - 3 Characters.png')}}" alt="Logo">
-    </div>
-</div>
 @endsection
