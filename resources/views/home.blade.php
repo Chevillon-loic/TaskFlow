0b5a91d43b539814ae30640d67d7bf20e91ee99e
Michael Santini <michael.santinimc@gmail.com>
2021-04-24 15:26:58 +0200

    Add migrate color for Board

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,34 +1,12 @@
 @extends('layouts.app')
 
+@section('custom_styles')
+    <link href="{{ asset('css/home.css') }}" rel="stylesheet">
+@endsection
+
 @section('content')
     <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">Dashboard</div>
+        <h1>Mes tableaux</h1>
 
-                    <div class="card-body">
-                        @if (session('status'))
-                            <div class="alert alert-success" role="alert">
-                                {{ session('status') }}
-                            </div>
-                        @endif
-
-                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
-                                document.getElementById('logout-form').submit();">
-                                {{ __('Logout') }}
-                            </a>
-
-                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                @csrf
-                            </form>
-
-
-                            You are logged in!
-                        </div>
-                    </div>
-                </div>
-            </div>
         </div>
     @endsection
