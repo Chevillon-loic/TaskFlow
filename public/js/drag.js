740d0d69001efb2f427c947ca7f4743d6374507b
Chaouki FREJ <chaoukifrej@hotmail.com>
2021-04-30 01:39:42 +0200

    design et modif du Drag and drop, simplification de drag.js et amélioration du drag and drop

--- public/js/drag.js
+++ public/js/drag.js
@@ -1,63 +1,80 @@
-//ELEMENT A DEPLACER (TICKET)
 function onDragStart(event) {
     event.dataTransfer.setData("text/plain", event.target.id);
+    setTimeout(() => {
+        event.target.classList.add("drag-over");
+    }, 0);
 }
 
 function onDragOver(event) {
     event.preventDefault();
+    setTimeout(() => {
+        event.target.classList.add("drag-over");
+    }, 0);
+    let ticket = document.querySelectorAll(".ticketDropZone");
+    ticket.forEach(elem => {
+        elem.style.height = "20px";
+        elem.style.borderRadius = "5px";
+        elem.style.margin = "10px 0";
+        elem.style.backgroundColor = "rgb(200, 200, 200)";
+    });
 }
 
-//LA DROPZONE (COLONNE)
+function onDragLeave(event) {
+    event.preventDefault();
+
+    setTimeout(() => {
+        event.target.classList.remove("drag-over");
+    }, 0);
+}
+async function onDrop(event) {
+    setTimeout(() => {
+        event.target.classList.remove("drag-over");
+    }, 0);
+    const id = event.dataTransfer.getData("text/plain");
+    const draggableElement = document.getElementById(id);
+
+    event.target.appendChild(draggableElement);
+
+    let idC = this.getAttribute("data_id");
+    let span = document.getElementById("span_data_url");
+    let url = span.getAttribute("data_url");
+    console.log(url);
+
+    let token = document
+        .querySelector('meta[name="csrf-token"]')
+        .getAttribute("content");
+
+    let body = {
+        column_id: idC
+    };
+
+    const options = {
+        method: "PUT",
+        headers: {
+            "X-CSRF-TOKEN": token,
+            Accept: "application/json",
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(body)
+    };
+
+    try {
+        const response = await fetch(url, options);
+        console.log(response);
+        // location.reload();
+    } catch (error) {
+        console.log(error);
+    }
+
+    event.dataTransfer.clearData();
+    location.reload();
+}
+
+//LA DROPZONE LISTENER (COLONNE)
 let ticket = document.querySelectorAll(".ticketDropZone");
 ticket.forEach(function(dropzone) {
-    dropzone.addEventListener("drop", async function(event) {
-        const id = event.dataTransfer.getData("text");
-        const draggableElement = document.getElementById(id);
-
-        let idC = dropzone.getAttribute("data_id");
-        dropzone.appendChild(draggableElement);
-        let span = document.getElementById("span_data_url");
-        let url = span.getAttribute("data_url");
-        console.log(url);
-
-        let token = document
-            .querySelector('meta[name="csrf-token"]')
-            .getAttribute("content");
-
-        let body = {
-            column_id: idC
-        };
-
-        const options = {
-            method: "PUT",
-            headers: {
-                "X-CSRF-TOKEN": token,
-                Accept: "application/json",
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(body)
-        };
-
-        try {
-            const response = await fetch(url, options);
-            console.log(response);
-            // location.reload();
-        } catch (error) {
-            console.log(error);
-        }
-
-        event.dataTransfer.clearData();
-    });
+    dropzone.addEventListener("dragenter", onDragStart);
+    dropzone.addEventListener("dragover", onDragOver);
+    dropzone.addEventListener("dragleave", onDragLeave);
+    dropzone.addEventListener("drop", onDrop);
 });
-
-/*
-
-  const draggables = document.querySelector('.boxTicket')
-  const dropzone = document.querySelectorAll('.ticketDropZone')
-
-  draggables.forEach(draggable => {
-    draggable.addEventListener('dragstart', () => {
-      draggable.classList.add('dragging')
-    })
-  })
-*/
